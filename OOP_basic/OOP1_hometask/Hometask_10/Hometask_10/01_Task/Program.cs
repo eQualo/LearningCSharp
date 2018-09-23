25da5f1a72194b432bc46a77fc3a9723714768f3
eQualo <37593019+eQualo@users.noreply.github.com>
2018-09-23 23:53:14 +0300

    Lesson 10 hometask Part1

---
+++ OOP_basic/OOP1_hometask/Hometask_10/Hometask_10/01_Task/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Task
+{
+	class MyClass<T> where T : new ()
+	{
+		public static T FactoryMethod()
+		{
+			return new T();
+		}
+	}
+	class MyClass1
+	{
+		public static MyClass<MyClass1> Method()
+		{
+			return new MyClass<MyClass1>();
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyClass<int>.FactoryMethod();
+			int k = MyClass<int>.FactoryMethod();
+			Console.WriteLine(k.GetType().Name);
+
+			MyClass1 my = MyClass<MyClass1>.FactoryMethod();
+			MyClass1.Method();
+		}
+	}
+}
---
+++ OOP_basic/OOP1_hometask/Hometask_10/Hometask_10/02_Task/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Task
+{
+	public interface IMyList<T> //Интерфейсы также могут быть параметризированными
+	{
+		void Add(T a); //Сигнатура параметризированного метода Add
+		T this[int index] { get; } //Сигнатура индексатора
+		int Count { get; } //Сигнатура свойства Count
+		void Clear(); //Сигнатура метода Clear
+		//bool Contains(T item); //Сигнатура метода-предиката
+	}
+
+	class MyList<T> : IMyList<T>
+	{
+		private int count = 0;
+		public int Count { get { return this.count; } }
+
+		private T[] elements;
+		//public T[] Elements { get { return this.elements; } }
+
+		public MyList(int count)
+		{
+			this.elements = new T[count];
+			this.count = count;
+		}
+
+		public T this[int index]
+		{
+			get { return this.elements[index]; }
+		}
+
+		public void Add(T value)
+		{
+			T[] buf = new T[this.Count + 1];
+			for(int i =0; i<this.Count; i++)
+			{
+				buf[i] = this.elements[i];
+			}
+			buf[this.Count] = value;
+			this.elements = buf;
+			this.count++;
+		}
+
+		public void Clear()
+		{
+			this.elements = new T[0];
+			this.count = 0;
+		}
+
+		public override string ToString()
+		{
+			string str = null;
+			for(int i=0;i<this.Count;i++)
+			{
+				str += this.elements[i].ToString() + " ";
+			}
+			return "Массив из " + this.Count.ToString() + " элементов. Элементы массива: " + str;
+		}
+		//public bool Contains(T item)
+		//{
+		//	for(int i =0;i<this.Count;i++)
+		//	{
+		//		if (this.elements[i] == item)
+		//			return true;
+		//	}
+		//}
+
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyList<double> myList = new MyList<double>(3);
+			Console.WriteLine(myList.ToString());
+			myList.Add(123.12);
+			myList.Add(52);
+			Console.WriteLine(myList.ToString());
+			myList.Clear();
+			Console.WriteLine(myList.ToString());
+			
+		}
+	}
+}
